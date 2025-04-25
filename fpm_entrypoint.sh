fac529ad9f3511055b7b0bbf36d3346bc088938d
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-26 00:19:18 +0200

    working

--- fpm_entrypoint.sh
+++ fpm_entrypoint.sh
@@ -13,16 +13,22 @@ if [ ! -f /var/www/html/public/index.php ]; then
   chown -R www-data:www-data /var/www/html
 fi
 
-# Patch trustedHostsPattern if settings.php exists
-if [ -n "$TRUSTED_HOST_PATTERN" ] && [ -f /var/www/html/config/system/settings.php ]; then
-  echo "Patching trustedHostsPattern to $TRUSTED_HOST_PATTERN..."
+# Patch settings.php if it exists
+if [ -f /var/www/html/config/system/settings.php ]; then
+  echo "Patching /var/www/html/config/system/settings.php..."
   # shellcheck disable=SC2016
   php -r '
     $cfgFile = "/var/www/html/config/system/settings.php";
     $cfg = include $cfgFile;
     $cfg["SYS"]["trustedHostsPattern"] = getenv("TRUSTED_HOST_PATTERN");
+    $cfg["SYS"]["displayErrors"] = getenv("TYPO3_DISPLAY_ERRORS");
+    $cfg["BE"]["entryPoint"] = getenv("TYPO3_BACKEND_PATH");
     file_put_contents($cfgFile, "<?php return " . var_export($cfg, true) . ";");
   '
+
+  # Clear TYPO3 caches after patch
+  echo "Clearing TYPO3 caches..."
+  rm -rf /var/www/html/var/cache/*
 fi
 
 exec php-fpm
